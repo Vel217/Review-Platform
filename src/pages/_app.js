7fc08e51ded619963a9c5f6d3fd30fba0be58e37
Vel217 <121238799+Vel217@users.noreply.github.com>
2023-04-27 18:23:42 +0400

    first commit: deploy4: add auth and button login

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import "react-quill/dist/quill.snow.css";
 import Layout from "../components/Layout/Layout";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import TwitterProvider from "next-auth/providers/twitter";
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
+export default NextAuth(authOptions);
