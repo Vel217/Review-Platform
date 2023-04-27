e966781751a122291d9cc63fc2814ef414fc5c39
Vel217 <121238799+Vel217@users.noreply.github.com>
2023-04-27 15:37:51 +0400

    first commit before deploy2

--- src/pages/review/index.js
+++ src/pages/review/index.js
@@ -109,7 +109,7 @@ function Review() {
             />
             <button
               type="button"
-              onClick={click}
+              // onClick={click}
               class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             >
               Add new film
